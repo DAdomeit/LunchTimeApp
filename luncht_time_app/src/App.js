60a96f5313694da20d50c304cd29556013fdd55e
Dennnis Adomeit <dennis.adomeit@gmx.de>
2024-05-20 14:31:26 +0200

    Umbau auf Material UI

--- luncht_time_app/src/App.js
+++ luncht_time_app/src/App.js
@@ -5,36 +5,53 @@ import Order from './pages/Order';
 import AddOrder from './pages/AddOrder';
 import OrderAPI from './api/OrderApi';
 import React, { useState } from 'react';
+import { AppBar, Box, Button, IconButton, ThemeProvider, Toolbar, createTheme } from '@mui/material';
+import { Add } from '@mui/icons-material';
 
 export const UserContext = React.createContext(null);
 
 function App() {
-  OrderAPI.initDemoData();
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#B81F40'
+      }, secondary: {
+        main: '#A6A6A'
+      }
+    }
+  });
   const [user, setUser] = useState();
+  
+  OrderAPI.initDemoData();
   if (user === undefined) {
     setUser(OrderAPI.getUser(localStorage.getItem("CurrentUserId")));
   }
 
   return (
     <HashRouter>
-      <div className="App">
-        <UserContext.Provider value={{ user: user, setUser: setUser }}>
-          <header className="App-header">
-            <ul className="header">
-              <li><NavLink to="/">Bestellungen</NavLink></li>
-              <li><NavLink to="/order">Neue Bestellung</NavLink></li>
-            </ul>
-          </header>
-          <div className='App-content'>
-            <Routes>
-              <Route exact path='/' element={ <OrderList /> }/>
-              <Route exact path='/order' element={ <Order /> }/>
-              <Route exact path='/order/:id' Component={ Order }/>
-              <Route exact path='/addorder/:orderid' Component={ AddOrder }/>
-            </Routes>
-          </div>
-        </UserContext.Provider>
-      </div>
+      <Box className="App">
+        <ThemeProvider theme={theme}>
+          <UserContext.Provider value={{ user: user, setUser: setUser }}>
+            <AppBar position='static' className='App-header' sx={{
+              background: '#b81f40',
+              color: 'white'
+            }}>
+              <Toolbar>
+                <Button component={NavLink} to='/' >Bestellungen</Button>
+                <IconButton component={NavLink} to='/order' ><Add/></IconButton>
+              </Toolbar>
+            </AppBar>
+            <div className='App-content'>
+              <Routes>
+                <Route exact path='/' element={ <OrderList /> }/>
+                <Route exact path='/order' element={ <Order /> }/>
+                <Route exact path='/order/:id' Component={ Order }/>
+                <Route exact path='/addorder/:orderid' Component={ AddOrder }/>
+              </Routes>
+            </div>
+          </UserContext.Provider>
+        </ThemeProvider>
+      </Box>
     </HashRouter>
   );
 }
